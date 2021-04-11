b35463cec4e1de45488966251e4d56ee9ea2a997
rgrace9 <rgracewilliams@gmail.com>
2021-04-11 15:24:54 -0400

    Update database config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '557a0ad0672c2b070e90aca03150c80c'),
+module.exports = ({ env }) => {
+  console.log(env.int('PORT'))
+
+  return ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    admin: {
+      auth: {
+        secret: env('ADMIN_JWT_SECRET', '557a0ad0672c2b070e90aca03150c80c'),
+      },
     },
-  },
-});
+  })
+};
