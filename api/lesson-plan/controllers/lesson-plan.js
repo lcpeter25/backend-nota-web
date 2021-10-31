66eee0e0faa9c3f0668f4391f3baa2ccf7309afb
rgrace9 <rgracewilliams@gmail.com>
2021-10-30 23:20:09 -0400

    Update the transcriptions and lesson plans response.

--- api/lesson-plan/controllers/lesson-plan.js
+++ api/lesson-plan/controllers/lesson-plan.js
@@ -1,8 +1,33 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services['lesson-plan'].search(ctx.query);
+    } else {
+      entities = await strapi.services['lesson-plan'].find(ctx.query);
+    }
+    return entities.map(entity => {
+      const lessonPlans = sanitizeEntity(entity, {
+        model: strapi.models['lesson-plan'],
+      });
+      
+      lessonPlans.authors = lessonPlans.authors.map(author => {
+
+        return {
+          name: author.name,
+          id: author.id
+        }
+      })
+      
+      delete lessonPlans.published_at;
+      delete lessonPlans.created_at;
+      delete lessonPlans.updated_at;
+
+      return lessonPlans;
+    });
+  },
+};
--- api/transcription/controllers/transcription.js
+++ api/transcription/controllers/transcription.js
@@ -1,8 +1,28 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.transcription.search(ctx.query);
+    } else {
+      entities = await strapi.services.transcription.find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const transcription = sanitizeEntity(entity, {
+        model: strapi.models.transcription,
+      });
+      
+      transcription.author = transcription.author ? transcription.author.name : null
+      
+      delete transcription.published_at;
+      delete transcription.created_at;
+      delete transcription.updated_at;
+
+      return transcription;
+    });
+  },
+};
