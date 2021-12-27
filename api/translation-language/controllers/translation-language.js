a639db66a4a1a3c07c0359b878976484de3ab838
Rebecca Williams <rgracewilliams@gmail.com>
2021-12-27 12:56:52 -0500

    Customize the response for the translation languages.

--- api/translation-language/controllers/translation-language.js
+++ api/translation-language/controllers/translation-language.js
@@ -1,8 +1,44 @@
 'use strict';
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services['translation-language'].search(ctx.query);
+    } else {
+      entities = await strapi.services['translation-language'].find(ctx.query);
+    }
+
+    return entities.map(entity => {
+      const translationLanguage = sanitizeEntity(entity, {
+        model: strapi.models['translation-language'],
+      });
+      
+      delete translationLanguage.translations
+      delete translationLanguage.published_at;
+      delete translationLanguage.created_at;
+      delete translationLanguage.updated_at;
+
+      return translationLanguage;
+    });
+  },
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    const entity = await strapi.services['translation-language'].findOne({id});
+
+    const sanitizedEntity = await sanitizeEntity(entity, {
+      model: strapi.models['translation-language'],
+    });
+
+    delete sanitizedEntity.translations
+    delete sanitizedEntity.published_at;
+    delete sanitizedEntity.created_at;
+    delete sanitizedEntity.updated_at;
+
+
+    return sanitizedEntity;
+  },
+};
